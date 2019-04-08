afdeb41377d259d0958ac194462d43622ea35569
dadadamarine <dadadamarine@gmail.com>
2019-04-08 22:27:25 +0900

    feature : 공통 칼럼인 id, createAt, modifiedAt을 위한 AbstractEntity추가

--- src/main/java/codesquad/BaeminchanApplication.java
+++ src/main/java/codesquad/BaeminchanApplication.java
@@ -2,8 +2,13 @@ package codesquad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan({"codesquad", "support"})
 public class BaeminchanApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaeminchanApplication.class, args);
--- src/main/java/codesquad/domain/Account.java
+++ src/main/java/codesquad/domain/Account.java
@@ -1,6 +1,7 @@
 package codesquad.domain;
 
 import lombok.Getter;
+import support.domain.AbstractEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,12 +10,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
 @Entity
-@Getter
-public class Account {
-
-    @Id
-    @GeneratedValue
-    private Long id;
+public class Account extends AbstractEntity {
 
     @Size(min = 3, max = 20)
     @Column(unique = true, nullable = false)
@@ -25,6 +21,52 @@ public class Account {
     private String password;
 
     @Column(nullable = false)
+    private String name;
+
+    @Size(max = 50)
+    @Column
+    private String email;
+
+    @Column(nullable = false)
     private Long type = 1l;
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getType() {
+        return type;
+    }
+
+    public void setType(Long type) {
+        this.type = type;
+    }
 }
