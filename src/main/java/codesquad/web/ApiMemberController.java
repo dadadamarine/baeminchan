84d7131be19f0ea8c230f132ce32870dd5a17bd9
정민석 <dadadamarine@gmail.com>
2019-05-22 16:48:36 +0900

    feature : 자동배포 테스트

--- src/main/java/codesquad/web/ApiMemberController.java
+++ src/main/java/codesquad/web/ApiMemberController.java
@@ -31,6 +31,7 @@ public class ApiMemberController {
     @PostMapping("")
     public ResponseEntity<Void> createMember(@Valid @RequestBody AccountRegistrationDTO accountRegistrationDTO) {
         accountService.save(accountRegistrationDTO);
+
         return makeDefaultResponseEntity("/login", HttpStatus.CREATED);
     }
 
